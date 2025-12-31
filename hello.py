a0de696ca1c63d68852c48086371833d5fbd057b
rifat-54 <rifat999por@gmail.com>
2026-01-01 01:36:13 +0600

    explore comment in python

--- hello.py
+++ hello.py
@@ -1,3 +1,16 @@
-name= input("What's your name?")
-print("hello",name)
+# name= input("What's your name?")
+# print("hello",name)
 # print(name)
+'''
+keso valo lage na
+'''
+
+"""
+its second style of comment
+"""
+
+
+# print("amr nam \n rifat")
+
+# print("my name \" rifat\"")
+print('my name \'rifat\'')
