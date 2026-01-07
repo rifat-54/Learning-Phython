ae498212922eed83b72b62dd2c74f3630eebad8e
rifat-54 <rifat999por@gmail.com>
2026-01-08 03:05:07 +0600

    explore string indexing

--- apn/lec2.py
+++ apn/lec2.py
@@ -1,20 +1,27 @@
-str1="this is string"
-str2='this is string'
-str3=""" this is 
-string
-"""
+# str1="this is string"
+# str2='this is string'
+# str3=""" this is 
+# string
+# """
 
-a="my name\"s is rifat"
+# a="my name\"s is rifat"
 
 # ! concatenation
 
-a="hello"
-b="world"
-print(len(a))
-print(len(b))
+# a="hello"
+# b="world"
+# print(len(a))
+# print(len(b))
 
 
-final_str=a+" "+b
-print(final_str)
-print(len(final_str))
+# final_str=a+" "+b
+# print(final_str)
+# print(len(final_str))
+
+
+# ! string
+
+str="hello world"
+
+print(str[-5:-2])
 
--- main.py
+++ main.py
@@ -67,15 +67,22 @@ num=10
 
 # print(int(a)+int(b))
 
-a="hello, i\"m  rifat"
+# a="hello, i\"m  rifat"
 
-b=""" hello i"m rifat
-i am form bd
- i live in dhaka
+# b=""" hello i"m rifat
+# i am form bd
+#  i live in dhaka
 
-"""
-# print(a[3])
+# """
+# # print(a[3])
 
-for char in b:
-    print(char)
+# for char in b:
+#     print(char)
+
+# fruit="mango"
+
+# print(fruit[:-8])
+
+# nm="Harry"
+# print(nm[-4:-2])
 
