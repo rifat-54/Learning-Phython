06fece1ff6a9fcfe02029512cd9e35f9e807f1b6
rifat-54 <rifat999por@gmail.com>
2026-01-10 01:04:33 +0600

    explore python if else

---
+++ condition.py
@@ -0,0 +1,28 @@
+# a=int(input("enter your age: "))
+
+# print("your age",a)
+
+# if(a>18):
+#     print("you can drive")
+#     print("yes")
+# else:
+#     print("your can not drive")
+#     print("no")
+
+# print("yeah!")
+
+
+num=int(input("enter a number: "))
+
+if(num>0):
+    
+    if(num==999):
+        print("lucky number!")
+    else:
+        print("number is positive")
+elif(num==0):
+    print("number is zero")
+else:
+    print("number is negative")
+
+print("I'm happy now!")
