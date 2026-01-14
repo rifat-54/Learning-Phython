7246e6cd0d533b94f1bdf65832b376790bf1fd4a
rifat-54 <rifat999por@gmail.com>
2026-01-15 01:46:21 +0600

    explore loop

---
+++ loop1.py
@@ -0,0 +1,16 @@
+# name="abhickes"
+# for i in name:
+#     print(i)
+#     if(i=='b'):
+#         print("this is special char!")
+
+# colors=["red","green","blue","yellow"]
+
+# for color in colors:
+#     for i in color:
+#         print(i,end=" ")
+#     print("\n")
+
+
+for k in range(1,20001):
+    print(k)
