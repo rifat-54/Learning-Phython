9d97717ca282e7a5d3bff616d8aac387fef2e864
rifat-54 <rifat999por@gmail.com>
2026-01-20 02:34:37 +0600

    explore dictionary

---
+++ dictionary.py
@@ -0,0 +1,29 @@
+info={
+    "name":"astha",
+    "subject":["python","C","Go"],
+    "topics":("dict","set"),
+    "age":34,
+    "is_adult":True,
+    12:343.34
+}
+
+# print(type(info))
+
+# info["name"]=45
+# info["surname"]="fatacasto"
+# print(info)
+
+# null_dict={}
+# null_dict["name"]="rifat"
+# print(null_dict)
+
+student={
+    "name":"rifat",
+    "subject":{
+        "phy":34,
+        "chem":54,
+        "math":46
+    }
+}
+
+print(student["subject"]["chem"])
