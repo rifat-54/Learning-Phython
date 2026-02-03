3998f493b93287ed44196ee34cb0b7654ad77ceb
rifat-54 <rifat999por@gmail.com>
2026-02-04 01:30:40 +0600

    explore list

--- cat.py
+++ cat.py
@@ -9,5 +9,16 @@
 # for _ in range(3):
 #     print("meow")
 
-print("meow\n"*3,end="")
+# print("meow\n"*3,end="")
+
+while True:
+    n=int(input("What's n? "))
+    if n>0:
+        break
+
+for _ in range(n):
+    print("meow")
+
+
+print("finished process")
 
---
+++ list.py
@@ -0,0 +1,3 @@
+lst=[i for i in range(9) if i%2==0]
+
+print(lst[1:8:2])
