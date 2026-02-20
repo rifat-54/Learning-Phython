a73a0738dc76d5f4a67e6c609cdb2239f8cc0671
rifat-54 <rifat999por@gmail.com>
2026-02-20 16:23:00 +0600

    explore list method

--- cp.cpp
+++ cp.cpp
@@ -1,8 +1,11 @@
-#include <iostream>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-int main() {
-    cout << "Hello, World! na" << endl;
+int main()
+{
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+   
+   cout<<"hello worls";
+      
     return 0;
 }
---
+++ list1.py
@@ -0,0 +1,25 @@
+l=[23,5,15,2,6,2]
+print(l)
+# l.append(7)
+# l.sort()
+# l.sort(reverse=True)
+
+# l.reverse()
+# print(l.index(2))
+# print(l.count(6))
+
+l.insert(1,899)
+
+# m=l 
+# m=l.copy()
+# m[0]=0
+# print(m)
+
+
+# ! extend 
+m=[343,564,657]
+# l.extend(m)
+
+k=l+m
+print(k)
+print(l)
